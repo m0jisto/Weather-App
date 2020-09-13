61eec58b8f1a396ed28f6bd09c42789af7ee2012
Nikita <cheremuhinikita@mail.ru>
2020-09-13 12:40:08 +0300

    fourth commit: add Hooks

--- src/components/cityWeather/cityWeather.js
+++ src/components/cityWeather/cityWeather.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import weatherService from'../../services/weatherService';
 import Spinner from '../spinner/'
@@ -6,91 +6,86 @@ import Spinner from '../spinner/'
 import './cityWeather.sass'
 import arrow from './arrow.png'
 
-class CityWeather extends Component {
-    state = {
-        weather: null,
-        loading: false
-    }
+const CityWeather = ({city}) => {
+    const [weather, updateWeather] = useState(null),
+    [loading, updateLoading]= useState(false);
 
-    weatherService = new weatherService()
+    useEffect(() => {
+        if (city !== '') {
+            updateLoading(true)
 
-    componentDidUpdate (prevProps) {
-        if (this.props.city !== prevProps.city && this.props.city !== null) {
-            this.setState({loading: true})
+            const service = new weatherService ()
             
-            this.weatherService.getWeather(this.props.city)
-                .then(weather => this.setState({
-                    weather: this.weatherService._transform(weather.data),
-                    loading: false
-                }))
-                .catch(() => this.setState({
-                    weather: undefined,
-                    loading: false
-                }))
-        } 
+            service.getWeather(city)
+                .then(weather => {
+                    updateWeather(service._transform(weather.data))
+                    updateLoading(false)
+                })
+                .catch(() => {
+                    updateWeather('error');
+                    updateLoading(false)
+                })
+        }
+    }, [city])
+
+    if (!weather) {
+        return <div></div>
     }
 
-    render () {
-        if (this.state.weather === null) {
-            return <div></div>
-        }
+    if (loading) {
+        return <Spinner />
+    }
 
-        if (this.state.loading) {
-            return <Spinner />
-        }
-
-        if (this.state.weather === undefined) {
-            return <h2 className="weather__error">Введите корректное название города</h2>
-        }
+    if (weather === 'error') {
+        return <h2 className="weather__error">Введите корректное название города</h2>
+    }
 
 
-        let minutes = new Date().getMinutes().toString();
+    let minutes = new Date().getMinutes().toString();
 
-        const hours = new Date().getHours().toString(),
-        month = new Date().getMonth().toString(),
-        date = new Date().getDate().toString(),
-        year = new Date().getFullYear().toString(),
-        months = ['Jan', 'Feb', 'Mar, Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const hours = new Date().getHours().toString(),
+    month = new Date().getMonth().toString(),
+    date = new Date().getDate().toString(),
+    year = new Date().getFullYear().toString(),
+    months = ['Jan', 'Feb', 'Mar, Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-        if (+minutes < 10) {
-            minutes = '0' + minutes
-        }
+    if (+minutes < 10) {
+        minutes = '0' + minutes
+    }
 
-        let {name, country, description, temp, wind, deg, feelTemp, humidity, pressure, visibility} = this.state.weather
+    let {name, country, description, temp, wind, deg, feelTemp, humidity, pressure, visibility} = weather
 
-        const src = `./icon/${this.state.weather.description}.png`
-        const styled = {
-            width: '15px',
-            height: '15px',
-            marginLeft: '5px',
-            transform: `rotate(${deg}deg)`
-        }
+    const src = `./icon/${description}.png`,
+    styled = {
+        width: '15px',
+        height: '15px',
+        marginLeft: '5px',
+        transform: `rotate(${deg}deg)`
+    }
 
-        description = description[0].toUpperCase() + description.slice(1)
+    description = description[0].toUpperCase() + description.slice(1)
 
-        return (
-            <div className="weather">
-                <div className="weather__header">
-                    <div>
-                        <div className="weather__date">{hours}:{minutes}, {months[month-1]} {date} {year}</div>
-                        <div className="weather__city">{name}, {country}</div>
-                    </div>
-
-                    <div className="weather__right">
-                        <img src={src} alt=''/>
-                        <div className="weather__temp">{temp}°C</div>
-                    </div>
+    return (
+        <div className="weather">
+            <div className="weather__header">
+                <div>
+                    <div className="weather__date">{hours}:{minutes}, {months[month-1]} {date} {year}</div>
+                    <div className="weather__city">{name}, {country}</div>
                 </div>
 
-                <div className="weather__wind">Wind: {wind}m/s <img style={styled} src={arrow} alt=''/></div>
-                <div className="weather__value">Feels like {feelTemp}°C. {description}</div>
-                <div className="weather__value">Humidity: {humidity}%</div>
-                <div className="weather__value">Pressure: {pressure}hPa</div>
-                <div className="weather__value">Visibility: {visibility}.0km</div>
-                
+                <div className="weather__right">
+                    <img src={src} alt=''/>
+                    <div className="weather__temp">{temp}°C</div>
+                </div>
             </div>
-        )
-    }
+
+            <div className="weather__wind">Wind: {wind}m/s <img style={styled} src={arrow} alt=''/></div>
+            <div className="weather__value">Feels like {feelTemp}°C. {description}</div>
+            <div className="weather__value">Humidity: {humidity}%</div>
+            <div className="weather__value">Pressure: {pressure}hPa</div>
+            <div className="weather__value">Visibility: {visibility}.0km</div>  
+        </div>
+    )
 }
 
 function mapStateToProps (state) {
--- src/components/searchCity/searchCity.js
+++ src/components/searchCity/searchCity.js
@@ -1,42 +1,35 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { changeNameCity } from '../../actions/'
 
 import './searchCity.sass'
 
-class SearchCity extends Component {
-    state = {
-        text: ''
-    }
+const SearchCity = ({changeNameCity}) => {
+    const [text, onChangeText] = useState('');
 
-    onSubmit = (e) => {
+    const onSubmit = (e) => {
         e.preventDefault()
-        this.props.changeNameCity('city', this.state.text)
-        this.setState({
-            text: ''
-        })
+        changeNameCity('city', text)
+        onChangeText('')
     }
 
-    render() {
-        return ( 
-            <form 
-                className="form"
-                onSubmit={this.onSubmit}>
-                <input
-                    className="form__input"
-                    type="text"
-                    placeholder="Введите название города"
-                    value={this.state.text}
-                    onChange={(e) => this.setState({text: e.target.value})}/>
-                <button
-                    className="form__btn"
-                    type="submit">Найти</button>
-            </form>
-        )
-    }
+    return ( 
+        <form 
+            className="form"
+            onSubmit={onSubmit}
+            >
+            <input
+                className="form__input"
+                type="text"
+                placeholder="Введите название города"
+                value={text}
+                onChange={(e) => onChangeText(e.target.value)}/>
+            <button
+                className="form__btn"
+                type="submit">Найти</button>
+        </form>
+    )
 }
-
-const mapStateToProps = (state) => state
 
 const mapDispatchToProps = (dispatch) => {
     return {
@@ -46,4 +39,4 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(SearchCity);
+export default connect(state => state, mapDispatchToProps)(SearchCity);
